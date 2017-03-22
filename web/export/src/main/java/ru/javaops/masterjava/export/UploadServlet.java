7e7c41211de773431b0a940dc05bce8f2700791f
Tanechka <truefirefox@gmail.com>
2017-03-21 23:11:23 -0700

    4 2 HW3 thymeleaf upload 4 3 HW3

---
+++ web/export/src/main/java/ru/javaops/masterjava/export/UploadServlet.java
@@ -0,0 +1,48 @@
+package ru.javaops.masterjava.export;
+
+import org.thymeleaf.context.WebContext;
+import ru.javaops.masterjava.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import static ru.javaops.masterjava.export.ThymeleafListener.engine;
+
+@WebServlet("/")
+@MultipartConfig
+public class UploadServlet extends HttpServlet {
+
+    private final UserExport userExport = new UserExport();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final WebContext webContext = new WebContext(req, resp, req.getServletContext(), req.getLocale());
+        engine.process("export", webContext, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final WebContext webContext = new WebContext(req, resp, req.getServletContext(), req.getLocale());
+
+        try {
+//            http://docs.oracle.com/javaee/6/tutorial/doc/glraq.html
+            Part filePart = req.getPart("fileToUpload");
+            try (InputStream is = filePart.getInputStream()) {
+                List<User> users = userExport.process(is);
+                webContext.setVariable("users", users);
+                engine.process("result", webContext, resp.getWriter());
+            }
+        } catch (Exception e) {
+            webContext.setVariable("exception", e);
+            engine.process("exception", webContext, resp.getWriter());
+        }
+    }
+}
