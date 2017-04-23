8b2f9a01b77abdc00bcad9252566447671f7aac7
Tanechka <truefirefox@gmail.com>
2017-04-22 17:39:12 -0700

    NoClassDefFoundError MailConfig

--- web/webapp/src/main/java/ru/javaops/masterjava/webapp/UsersServlet.java
+++ web/webapp/src/main/java/ru/javaops/masterjava/webapp/UsersServlet.java
@@ -1,9 +1,13 @@
 package ru.javaops.masterjava.webapp;
 
+import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableMap;
 import org.thymeleaf.context.WebContext;
 import ru.javaops.masterjava.persist.DBIProvider;
 import ru.javaops.masterjava.persist.dao.UserDao;
+import ru.javaops.masterjava.persist.model.User;
+import ru.javaops.masterjava.service.mail.Addressee;
+import ru.javaops.masterjava.service.mail.MailWSClient;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +15,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static ru.javaops.masterjava.common.web.ThymeleafListener.engine;
 
@@ -23,5 +31,18 @@ public class UsersServlet extends HttpServlet {
         final WebContext webContext = new WebContext(req, resp, req.getServletContext(), req.getLocale(),
                 ImmutableMap.of("users", userDao.getWithLimit(20)));
         engine.process("users", webContext, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doPost(req, resp);
+        //List<Integer> ids = new ArrayList<>();
+        List<Integer> ids = req.getReader().lines()
+                .map(line -> Splitter.on(',').splitToList(line))
+                .flatMap(list -> list.stream().map(Integer::valueOf))
+                .collect(Collectors.toList());
+        List<User> users = userDao.getByIds(ids);
+        Set<Addressee> to = users.stream().map(u -> new Addressee(u.getEmail(), u.getFullName())).collect(Collectors.toSet());
+        MailWSClient.sendMail(to, Collections.EMPTY_SET, "testSubject", "testBody");
     }
 }
