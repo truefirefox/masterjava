5af615e3473765c8b345fd98a83106f81ff1f5c7
Tanechka <truefirefox@gmail.com>
2017-05-01 23:21:02 -0700

    9 5 msg ctx auth

---
+++ services/common-ws/src/main/java/ru/javaops/web/AuthUtil.java
@@ -0,0 +1,32 @@
+package ru.javaops.web;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.DatatypeConverter;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class AuthUtil {
+    private static final String AUTHORIZATION = "Authorization";
+
+    public static String encodeBasicAuthHeader(String name, String passw) {
+        String authString = name + ":" + passw;
+        return "Basic " + DatatypeConverter.printBase64Binary(authString.getBytes());
+    }
+
+    public static int checkBasicAuth(Map<String, List<String>> headers, String basicAuthCredentials) {
+        List<String> autHeaders = headers.get(AUTHORIZATION);
+        if ((autHeaders == null || autHeaders.isEmpty())) {
+            log.warn("Unauthorized access");
+            return HttpServletResponse.SC_UNAUTHORIZED;
+        } else {
+            if (!autHeaders.get(0).equals(basicAuthCredentials)) {
+                log.warn("Wrong password access");
+                return HttpServletResponse.SC_FORBIDDEN;
+            }
+            return 0;
+        }
+    }
+}
--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceImpl.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceImpl.java
@@ -1,9 +1,14 @@
 package ru.javaops.masterjava.service.mail;
 
+import ru.javaops.web.AuthUtil;
 import ru.javaops.web.WebStateException;
 
+import javax.annotation.Resource;
 import javax.jws.WebService;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @WebService(endpointInterface = "ru.javaops.masterjava.service.mail.MailService", targetNamespace = "http://mail.javaops.ru/"
@@ -13,8 +18,22 @@ import java.util.Set;
 //@MTOM
 public class MailServiceImpl implements MailService {
 
+    @Resource
+    private WebServiceContext wsContext;
+
     @Override
     public String sendToGroup(Set<Addressee> to, Set<Addressee> cc, String subject, String body, List<Attach> attaches) throws WebStateException {
+        MessageContext mCtx = wsContext.getMessageContext();
+        Map<String, List<String>> headers = (Map<String, List<String>>) mCtx.get(MessageContext.HTTP_REQUEST_HEADERS);
+
+//        HttpServletRequest request = (HttpServletRequest) mCtx.get(MessageContext.SERVLET_REQUEST);
+//        HttpServletResponse response = (HttpServletResponse) mCtx.get(MessageContext.SERVLET_RESPONSE);
+
+        int code = AuthUtil.checkBasicAuth(headers, MailWSClient.AUTH_HEADER);
+        if (code != 0) {
+            mCtx.put(MessageContext.HTTP_RESPONSE_CODE, code);
+            throw new SecurityException();
+        }
         return MailSender.sendToGroup(to, cc, subject, body, attaches);
     }
 
