69b53778c9a7a4886dbbe1e7b88d0ce1b1663260
Tanechka <truefirefox@gmail.com>
2017-05-11 19:49:11 -0700

    add attaches for JAX-RS

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/rest/MailRS.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/rest/MailRS.java
@@ -1,15 +1,22 @@
 package ru.javaops.masterjava.service.mail.rest;
 
 
+import com.google.common.collect.ImmutableList;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.hibernate.validator.constraints.NotBlank;
+import ru.javaops.masterjava.service.mail.Attach;
 import ru.javaops.masterjava.service.mail.GroupResult;
 import ru.javaops.masterjava.service.mail.MailServiceExecutor;
 import ru.javaops.masterjava.service.mail.MailWSClient;
+import ru.javaops.masterjava.service.mail.util.Attachments;
 import ru.javaops.web.WebStateException;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.Collections;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 @Path("/")
 public class MailRS {
@@ -23,10 +30,25 @@ public class MailRS {
     @POST
     @Path("send")
     @Produces(MediaType.APPLICATION_JSON)
-    public GroupResult send(@NotBlank @FormParam("users") String users,
-                            @FormParam("subject") String subject,
-                            @NotBlank @FormParam("body") String body) throws WebStateException {
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public GroupResult send(@NotBlank @FormDataParam("users") String users,
+                            @FormDataParam("subject") String subject,
+                            @NotBlank @FormDataParam("body") String body,
+                            @FormDataParam("attach") InputStream uploadedInputStream,
+                            @FormDataParam("attach") FormDataContentDisposition fileDetail) throws WebStateException {
 
-        return MailServiceExecutor.sendBulk(MailWSClient.split(users), subject, body, Collections.emptyList());
+        List<Attach> attaches;
+        if (uploadedInputStream == null) {
+            attaches = ImmutableList.of();
+        } else {
+            String name = fileDetail.getFileName();
+            try {
+                name = new String(fileDetail.getFileName().getBytes ("iso-8859-1"), "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            attaches = ImmutableList.of(Attachments.getAttach(name, uploadedInputStream));
+        }
+        return MailServiceExecutor.sendBulk(MailWSClient.split(users), subject, body, attaches);
     }
 }
